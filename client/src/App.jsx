e58291c4c2c25bbd7e5f74024f2ac1e90d6091f7
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-05-15 11:42:13 +0100

    create Layouts and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,88 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import './App.css'
-import Home from './Pages/Home'
+import Layout from './components/Layout'
+import ErrorPage from "./Pages/ErrorPage"
+import Home from "./Pages/Home"
+import PostDetails from "./Pages/PostDetails"
+import Register from "./Pages/Register"
+import Login from "./Pages/Login"
+import Logout from "./Pages/Logout"
+import UserProfile from "./Pages/UserProfile"
+import EditPost from "./Pages/EditPost"
+import CreatePost from "./Pages/CreatePost"
+import Authors from "./Pages/Authors"
+import CategoryPosts from "./Pages/CategoryPosts"
+import Dashboard from "./Pages/Dashboard"
+import AuthorsPosts from './Pages/AuthorsPosts'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "posts/:id",
+        element: <PostDetails />
+      },
+      {
+        path: "register",
+        element: <Register />
+      },
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "logout",
+        element: <Logout />
+      },
+      {
+        path: "profile/:id",
+        element: <UserProfile />
+      },
+      {
+        path: "/posts/:id/edit",
+        element: <EditPost />
+      },
+      {
+        path: "authors",
+        element: <Authors />
+      },
+      {
+        path: "create",
+        element: <CreatePost />
+      },
+      {
+        path: "posts.categories/:category",
+        element: <CategoryPosts />
+      },
+      {
+        path: "myposts/:id",
+        element: <Dashboard />
+      },
+      {
+        path: "posts/users/:id",
+        element: <AuthorsPosts />
+      },
+
+
+  ]
+  }
+])
+
 
 const App = () => {
 
   return (
     <>
-
-      <Home/>
+    <RouterProvider router={router} />
     </>
   )
 }
